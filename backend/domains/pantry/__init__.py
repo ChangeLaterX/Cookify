24fd801600e2da47415f5b1c353e3a758d69f13a
Cipher Shad0w <Jannis.krija@icloud.com>
2025-06-12 14:12:22 +0200

    feat(pantry, receipt): Add models and initialization for pantry and receipt domains

---
+++ backend/domains/pantry/__init__.py
@@ -0,0 +1,12 @@
+from .models import PantryItem
+
+# from .routes import router
+
+__all__: list[str] = [
+    # Models
+    "PantryItem",
+    # Schemas
+    # Services
+    # Routes
+    # "router",
+]
---
+++ backend/domains/receipt/__init__.py
@@ -0,0 +1,12 @@
+from .models import IngredientMaster
+
+# from .routes import router
+
+__all__: list[str] = [
+    # Models
+    "IngredientMaster",
+    # Schemas
+    # Services
+    # Routes
+    #"router",
+]
