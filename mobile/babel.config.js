f5932aaa77dd80eb949336d51c81f085dd57de0f
Ashvin Panicker <agnjkafgh@gmail.com>
2025-06-16 03:25:22 +0530

    fix bug https://stackoverflow.com/questions/78494804/expo-stuck-on-welcome-to-expo-screen

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,21 +1,6 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: '.env',
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: true,
-        },
-      ],
-      // Add other plugins here if needed, e.g., 'react-native-reanimated/plugin'
-      // Ensure 'react-native-reanimated/plugin' is listed last if you use it.
-    ],
+    presets: ['babel-preset-expo']
   };
 };
